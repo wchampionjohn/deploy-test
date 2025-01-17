be4e07996f333c038347887b34e2c0949311a62c
John <vhitotoyouv@gmail.com>
2025-01-17 17:21:00 +0800

    extra redis config to config/redis.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,12 +4,23 @@ require "sidekiq"
 require "sidekiq-scheduler"
 require "sidekiq/web"
 
+def redis_url
+  @redis_url ||=
+    begin
+      rails_root = ENV["RAILS_ROOT"] || File.dirname(__FILE__) + "/../.."
+      rails_env = Rails.env || "development"
+      redis_host = YAML.load_file(rails_root + "/config/redis.yml")
+      redis_host[rails_env]
+    end
+end
+
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://127.0.0.1:6379/15" }
+  config.redis = { url: "redis://#{redis_url}" }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://127.0.0.1:6379/15" }
+  config.redis = { url: "redis://#{redis_url}" }
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file("config/sidekiq_scheduler.yml") || []
     SidekiqScheduler::Scheduler.instance.reload_schedule!
