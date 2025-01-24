2fbac208f88626f382b218820aff9d250c099507
John <vhitotoyouv@gmail.com>
2025-01-24 15:14:45 +0800

    added spec document format config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -95,4 +95,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # output format
+  config.formatter = :documentation
 end
